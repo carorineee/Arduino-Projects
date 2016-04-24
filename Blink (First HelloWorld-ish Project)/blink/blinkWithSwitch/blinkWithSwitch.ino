afdcff4ac575b088d91d53782870ff3ca5068a93
Caroline Shi <detectiveconan123456789@gmail.com>
2016-04-23 22:57:04 -0500

    Changed blink with switch

--- Blink (First HelloWorld-ish Project)/blink/blinkWithSwitch/blinkWithSwitch.ino
+++ Blink (First HelloWorld-ish Project)/blink/blinkWithSwitch/blinkWithSwitch.ino
@@ -1,11 +1,18 @@
 int ledPin = 9;
+int buttonPin = 12;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  int buttonState = digitalRead(buttonPin);
+  if(buttonState == 1) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);  
+    }
 }
